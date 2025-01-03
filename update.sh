966f5dd2a57a71c1d0edeec985932a12d80cc3e7
Patrick Quinn-Graham <7331+thepatrick@users.noreply.github.com>
2025-01-03 15:00:53 +1100

    update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 cd /opt/p2-network/label-printer-proxy/
 
 git pull
 
-launchctl bootstrap gui/$(id -u) nz.patrick.label-printer-proxy.plist
-
 launchctl bootout gui/$(id -u) nz.patrick.label-printer-proxy.plist
+
+launchctl bootstrap gui/$(id -u) nz.patrick.label-printer-proxy.plist
