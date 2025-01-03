0e505c785381d4b60bc4d16476aaf3724a742452
Patrick Quinn-Graham <7331+thepatrick@users.noreply.github.com>
2025-01-03 15:16:43 +1100

    update update.sh

--- update.sh
+++ update.sh
@@ -7,9 +7,11 @@ git pull
 # if launchd job is running
 if launchctl list | grep -q nz.patrick.label-printer-proxy; then
   echo "Unloading service..."
-  launchctl bootout gui/$(id -u) nz.patrick.label-printer-proxy.plist
+  launchctl bootout gui/$(id -u) ~/Library/LaunchAgents/nz.patrick.label-printer-proxy.plist
 fi
+
+cp nz.patrick.label-printer-proxy.plist ~/Library/LaunchAgents
 
 echo "Loading service..."
 
-launchctl bootstrap gui/$(id -u) nz.patrick.label-printer-proxy.plist
+launchctl bootstrap gui/$(id -u) ~/Library/LaunchAgents/nz.patrick.label-printer-proxy.plist
