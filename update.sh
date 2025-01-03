477735f67c283906d83227ae5e794b9ce0f9b81f
Patrick Quinn-Graham <7331+thepatrick@users.noreply.github.com>
2025-01-03 15:05:15 +1100

    update update.sh (2)

--- update.sh
+++ update.sh
@@ -4,6 +4,12 @@ cd /opt/p2-network/label-printer-proxy/
 
 git pull
 
-launchctl bootout gui/$(id -u) nz.patrick.label-printer-proxy.plist
+# if launchd job is running
+if launchctl list | grep -q nz.patrick.label-printer-proxy; then
+  echo "Unloading service..."
+  launchctl bootout gui/$(id -u) nz.patrick.label-printer-proxy.plist
+fi
+
+echo "Loading service..."
 
 launchctl bootstrap gui/$(id -u) nz.patrick.label-printer-proxy.plist
